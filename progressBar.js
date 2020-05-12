a534853ef19608ebe1e58535898d1f437a9e5c38
NerijusPazera <nerijus.pazera@gmail.com>
2020-05-12 13:24:39 +0300

    Java Script progrees bar'as ir userio ekrano vieta puslapyje

---
+++ progressBar.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const h1 = document.body.appendChild(document.createElement('h1'));
+const button = document.querySelector('button');
+
+button.addEventListener('click', e => {
+  scrollTo({top: 0, left: 0, behavior: 'smooth'});
+})
+
+document.addEventListener('scroll', () => {
+  const percentage = getScrollPercentage();
+  const progress = document.querySelector('.progress-bar');
+
+  progress.style.width = `${percentage}%`;
+
+  showButton(pageYOffset > innerHeight);
+  colorBody(Math.round(255 / 100 * percentage));
+});
+
+function getScrollPercentage() {
+  const bodyHeight = document.body.scrollHeight;
+  const maxScroll = bodyHeight - innerHeight;
+  return Math.round(pageYOffset * 100 / maxScroll);
+}
+
+function showButton(show) {
+  button.style.display = show ? 'block' : 'none';
+}
+
+function colorBody(number) {
+  document.body.style.backgroundColor = `rgb(${number}, ${number}, ${number})`;
+}
